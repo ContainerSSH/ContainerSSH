86cf644afeb5353cfa59aa2bb722a8ebce2cfcf5
Janos Pasztor <janos@pasztor.at>
2020-08-04 08:20:02 +0200

    Better description for log levels

--- config/log.go
+++ config/log.go
@@ -4,5 +4,6 @@ import "github.com/janoszen/containerssh/log"
 
 // swagger:model
 type AppLogConfig struct {
+	// Minimum log level to log. Everything below the specified level will not be logged. Can be changed from the configuration server.
 	Level log.LevelString `json:"level" yaml:"level" default:"info" comment:"Log level. Can be any valid Syslog log level."`
 }
