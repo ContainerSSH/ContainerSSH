b0638e6adf3114587daa5d8512c3b6589f014a65
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2022-03-20 18:41:09 +0000

    Single build pipeline + nightly builds (#426)

--- cmd/containerssh-auditlog-decoder/main.go
+++ cmd/containerssh-auditlog-decoder/main.go
@@ -1,73 +1,69 @@
 package main
 
 import (
-	"encoding/json"
-	"flag"
-	"fmt"
-	"log"
-	"os"
+    "encoding/json"
+    "flag"
+    "fmt"
+    "log"
+    "os"
 
-	"github.com/containerssh/auditlog/codec/binary"
+    "github.com/containerssh/auditlog/codec/binary"
 )
 
 func main() {
-	file := ""
-	flag.StringVar(&file, "file", "", "File to process")
-	flag.Parse()
-	if flag.NArg() != 0 {
-		flag.Usage()
-		os.Exit(1)
-	}
+    file := ""
+    flag.StringVar(&file, "file", "", "File to process")
+    flag.Parse()
+    if flag.NArg() != 0 {
+        flag.Usage()
+        os.Exit(1)
+    }
 
-	if file == "" {
-		flag.Usage()
-		os.Exit(1)
-	}
+    if file == "" {
+        flag.Usage()
+        os.Exit(1)
+    }
 
-	fh, err := os.Open(file)
-	if err != nil {
-		log.Fatalf("failed to open audit log file %s (%v)", file, err)
-	}
+    fh, err := os.Open(file)
+    if err != nil {
+        log.Fatalf("failed to open audit log file %s (%v)", file, err)
+    }
 
-	decoder := binary.NewDecoder()
-	messages, errors := decoder.Decode(fh)
+    decoder := binary.NewDecoder()
+    messages, errors := decoder.Decode(fh)
 loop:
-	for {
-		select {
-		case msg, ok := <-messages:
-			if !ok {
-				break loop
-			}
+    for {
+        select {
+        case msg, ok := <-messages:
+            if !ok {
+                break loop
+            }
 
-			var data []byte
-			data, err = json.Marshal(msg.GetExtendedMessage())
-			if err != nil {
-				structuredError := map[string]string{
-					"error": fmt.Sprintf("JSON encoding error: (%v)", err),
-				}
-				data, _ = json.Marshal(structuredError)
-				_, _ = os.Stdout.Write(data)
-				_, _ = os.Stdout.Write([]byte("\n"))
-			} else if data != nil {
-				_, _ = os.Stdout.Write(data)
-				_, _ = os.Stdout.Write([]byte("\n"))
-			} else {
-				break
-			}
-		case channelError, ok := <-errors:
-			if !ok {
-				break loop
-			}
-			if channelError != nil {
-				structuredError := map[string]string{
-					"error": channelError.Error(),
-				}
-				data, _ := json.Marshal(structuredError)
-				_, _ = os.Stdout.Write(data)
-				_, _ = os.Stdout.Write([]byte("\n"))
-			} else {
-				break
-			}
-		}
-	}
+            var data []byte
+            data, err = json.Marshal(msg.GetExtendedMessage())
+            if err != nil {
+                structuredError := map[string]string{
+                    "error": fmt.Sprintf("JSON encoding error: (%v)", err),
+                }
+                data, _ = json.Marshal(structuredError)
+                _, _ = os.Stdout.Write(data)
+                _, _ = os.Stdout.Write([]byte("\n"))
+            } else if data != nil {
+                _, _ = os.Stdout.Write(data)
+                _, _ = os.Stdout.Write([]byte("\n"))
+            }
+        case channelError, ok := <-errors:
+            if !ok {
+                break loop
+            }
+            if channelError != nil {
+                structuredError := map[string]string{
+                    "error": channelError.Error(),
+                }
+                data, _ := json.Marshal(structuredError)
+                _, _ = os.Stdout.Write(data)
+                _, _ = os.Stdout.Write([]byte("\n"))
+            }
+        }
+    }
 }
