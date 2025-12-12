3f80dd1e0116a36a6e6a00c77a22b723783fcadc
Nikos Tsipinakis <nikos@tsipinakis.com>
2025-12-12 23:36:47 +0100

    Replace reprint
    Signed-off-by: Nikos Tsipinakis <nikos@tsipinakis.com>

--- go.mod
+++ go.mod
@@ -76,6 +76,7 @@ require (
 	github.com/jcmturner/goidentity/v6 v6.0.1 // indirect
 	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
 	github.com/jdkato/prose v1.2.1 // indirect
+	github.com/jinzhu/copier v0.4.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
--- internal/structutils/copy.go
+++ internal/structutils/copy.go
@@ -1,8 +1,9 @@
 package structutils
 
-import "github.com/qdm12/reprint"
+import "github.com/jinzhu/copier"
 
 // Copy deep copies a struct pointer from source to destination.
 func Copy(destination interface{}, source interface{}) error {
-	return reprint.FromTo(source, destination)
+	err := copier.CopyWithOption(destination, source, copier.Option{IgnoreEmpty: true, DeepCopy: true})
+	return err
 }
