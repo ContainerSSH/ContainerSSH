8c3bb7479d17977a95d257b0dd18c6a9caeb2698
Nikos Tsipinakis <nikos@tsipinakis.com>
2026-01-14 18:50:59 +0100

    Remove deprecated DockerRun/KubeRun config validation
    Signed-off-by: Nikos Tsipinakis <nikos@tsipinakis.com>

--- internal/config/compatibility_test.go
+++ internal/config/compatibility_test.go
@@ -2,7 +2,6 @@ package config_test
 
 import (
 	"context"
-	"errors"
 	"os"
 	"testing"
 
@@ -35,62 +34,4 @@ func Test04Compatibility(t *testing.T) {
 	assert.NoError(t, err)
 	err = cfg.Validate(false)
 	assert.NoError(t, err)
-}
-
-// TestDockerRunRemoval tests if the removed DockerRun backend returns the correct error message.
-func TestDockerRunRemoval(t *testing.T) {
-	logger := log.NewTestLogger(t)
-
-	testFile, err := os.Open("_testdata/dockerrun.yaml")
-	assert.NoError(t, err)
-	reader, err := config.NewReaderLoader(
-		testFile,
-		logger,
-		config.FormatYAML,
-	)
-	assert.NoError(t, err)
-	cfg := configuration.AppConfig{}
-	structutils.Defaults(&cfg)
-	err = reader.Load(context.Background(), &cfg)
-	assert.NoError(t, err)
-	err = cfg.Validate(false)
-	if err == nil {
-		t.Fatalf("Validation of a configuration with the DockerRun backend did not fail.")
-	}
-	var typedError message.Message
-	if !errors.As(err, &typedError) {
-		t.Fatalf("Using the deprecated DockerRun backend did not result in a message.Message error.")
-	}
-	if typedError.Code() != message.EDockerRunRemoved {
-		t.Fatalf("Validation returned the wrong error code: %s", typedError.Code())
-	}
-}
-
-// TestKubeRunRemoval tests if the removed KubeRun backend returns the correct error message.
-func TestKubeRunRemoval(t *testing.T) {
-	logger := log.NewTestLogger(t)
-
-	testFile, err := os.Open("_testdata/kuberun.yaml")
-	assert.NoError(t, err)
-	reader, err := config.NewReaderLoader(
-		testFile,
-		logger,
-		config.FormatYAML,
-	)
-	assert.NoError(t, err)
-	cfg := configuration.AppConfig{}
-	structutils.Defaults(&cfg)
-	err = reader.Load(context.Background(), &cfg)
-	assert.NoError(t, err)
-	err = cfg.Validate(false)
-	if err == nil {
-		t.Fatalf("Validation of a configuration with the KubeRun backend did not fail.")
-	}
-	var typedError message.Message
-	if !errors.As(err, &typedError) {
-		t.Fatalf("Using the deprecated KubeRun backend did not result in a message.Message error.")
-	}
-	if typedError.Code() != message.EKubeRunRemoved {
-		t.Fatalf("Validation returned the wrong error code: %s", typedError.Code())
-	}
 }
